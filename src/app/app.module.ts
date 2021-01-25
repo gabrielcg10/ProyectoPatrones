8bdca79999ba8f1c99a5f82f3db6afcd0fca39ad
unknown <gchavezguerrero985@gmail.com>
2021-01-25 14:42:20 -0500

    version 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//rutas
+import { APP_ROUTING } from './app.routes';
+//servicios
+import { CursosService } from './services/cursos.service';
+//componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PagosComponent } from './components/pagos/pagos.component';
+import { CursoComponent } from './components/curso/curso.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CursosComponent,
+    NosotrosComponent,
+    InicioComponent,
+    BlogComponent,
+    FooterComponent,
+    ContactoComponent,
+    LoginComponent,
+    HeaderComponent,
+    PagosComponent,
+    CursoComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [
+    CursosService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent} from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { LoginComponent } from './components/login/login.component';
+import { CursoComponent } from './components/curso/curso.component';
+import { PagosComponent } from './components/pagos/pagos.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'cursos', component: CursosComponent },
+    { path: 'contacto', component: ContactoComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'curso/:id', component: CursoComponent },
+    { path: 'pagos', component: PagosComponent },
+    { path: '**', pathMatch:'full', redirectTo: 'Inicio' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
